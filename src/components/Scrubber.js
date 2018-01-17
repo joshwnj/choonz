2a96ae1583a3f4af61b4864d632cef1aa5889782
Josh Johnston <josh@x-team.com>
2018-01-18 10:56:55 +1100

    use html5 audio instead of webaudio

--- src/components/Scrubber.js
+++ src/components/Scrubber.js
@@ -1,14 +1,13 @@
 const html = require('choo/html')
 
-module.exports = function Scrubber ({ buffer, progress }, emit) {
-  if (!buffer || !progress) { return '' }
-  const { duration } = buffer
+module.exports = function Scrubber ({ audio }, emit) {
+  const { currentTime, duration } = audio
 
-  const percent = (progress / duration) * 100
+  const percent = (currentTime / duration) * 100
   return html`
   <div class='scrubber-wrapper'>
     <div style='position: relative'>
-      <input type='range' class='scrubber' min='0' max='${duration}' value=${progress} oninput=${(e) => emit('scrub', e.target.value)} onchange=${(e) => emit('seek', e.target.value)} />
+      <input type='range' class='scrubber' min='0' max='${duration}' value=${currentTime} oninput=${(e) => emit('scrub', e.target.value)} onchange=${(e) => emit('seek', e.target.value)} />
       <div class='train-wrapper'>
         <span class='train flip-h' style='left: ${percent}%;'>🚂</spn>
       </div>
