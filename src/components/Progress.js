3ebddc49e78c3c84b14a92f1e7cd3d329d7eb9f6
Josh Johnston <josh@x-team.com>
2018-03-30 11:07:50 -0400

    Catch error caused by invalid time
    Fixes #6

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -2,7 +2,11 @@ const html = require('choo/html')
 const hms = require('hh-mm-ss')
 
 function formatTime (seconds) {
-  return hms.fromS(Math.round(seconds), 'mm:ss')
+  try {
+    return hms.fromS(Math.round(seconds), 'mm:ss')
+  } catch (e) {
+    return '--:--'
+  }
 }
 
 module.exports = function Progress ({ audio }) {
